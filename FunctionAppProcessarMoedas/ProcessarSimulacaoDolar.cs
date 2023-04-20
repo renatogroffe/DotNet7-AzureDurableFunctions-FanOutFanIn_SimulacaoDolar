4f7ee6642d4d17c07dfc117b6039291d500cfaba
Renato Groffe <renato.groffe@yahoo.com.br>
2023-04-20 10:54:20 -0300

    .NET 7 + Azure Durable Functions (Fan-Out/Fan-In) + Isolated Process + Timer Starter + Azure Queue Storage + Azure Service Bus + Simulação Cotações Dólar
    .NET 7 + Azure Durable Functions (Fan-Out/Fan-In) + Isolated Process + Timer Starter + Azure Queue Storage + Azure Service Bus + Simulação Cotações Dólar

---
+++ FunctionAppProcessarMoedas/ProcessarSimulacaoDolar.cs
@@ -0,0 +1,32 @@
+﻿using FunctionAppProcessarMoedas.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionAppProcessarMoedas;
+
+public static class ProcessarSimulacaoDolar
+{
+    [Function(nameof(ProcessarSimulacaoDolar))]
+    public static async Task RunOrchestrator(
+        [OrchestrationTrigger] TaskOrchestrationContext context)
+    {
+        var logger = context.CreateReplaySafeLogger(nameof(ProcessarSimulacaoDolar));
+        logger.LogInformation(
+            $"{nameof(ProcessarSimulacaoDolar)} - Iniciando a execucao do metodo {nameof(RunOrchestrator)}...");
+
+        var parametrosExecucao = context.GetInput<ParametrosExecucao>();
+
+        var dadosCotacao = await context.CallActivityAsync<DadosCotacao>(
+            nameof(ActivitySimularCotacaoDolar), parametrosExecucao);
+        var tasks = new Task<bool>[2];
+        tasks[0] = context.CallActivityAsync<bool>(
+            nameof(ActivityNotificarAzureQueueStorage), dadosCotacao);
+        tasks[1] = context.CallActivityAsync<bool>(
+            nameof(ActivityNotificarAzureServiceBus), dadosCotacao);
+        await Task.WhenAll(tasks);
+
+        logger.LogInformation(
+            $"{nameof(ProcessarSimulacaoDolar)} - Concluindo a execucao do metodo {nameof(RunOrchestrator)}.");
+    }
+}
